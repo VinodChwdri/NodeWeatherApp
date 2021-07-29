c4fc57ef3bac937f66206bb1992db8ea1233bf26
vinod_2025 <vinodchaudharyjee@gmail.com>
2021-07-29 09:26:10 +0530

    Loader text added

--- public/js/client.js
+++ public/js/client.js
@@ -1,19 +1,22 @@
 
 const forms = document.querySelector('form')
 const searchElement = document.querySelector('input')
+const message = document.querySelector('#WeatherInfoId');
+
 
 forms.addEventListener('submit',(e)=>{
     e.preventDefault()
     const address = searchElement.value;
-    const url ='/weather?address='+address 
+    const url ='/weather?address='+address;
+    msg.textContent = "Loading...";
     fetch(url)
     .then((res)=>{
         if(res.error){
             console.log(res.error)
-            document.getElementById('WeatherInfoId').innerHTML = res.error
+            msg.textContent = res.error
         }else{
            res.json().then((data)=>{
-            document.getElementById('WeatherInfoId').innerHTML = JSON.stringify(data)
+            msg.textContent = JSON.stringify(data)
         })
     }
     })
